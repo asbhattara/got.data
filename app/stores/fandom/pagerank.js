b7974e0eb58ba38685fdd19d451406c946bf4b51
David Schemm <davidschemm95@gmail.com>
2019-03-17 16:54:13 +0100

    pagerank and religion stores added

--- app/stores/fandom/pagerank.js
+++ app/stores/fandom/pagerank.js
@@ -1,1 +1,35 @@
-// TODO:
+const PageRanks = require('../../models/fandom/pagerank')
+
+class PageRankStore {
+    constructor() {}
+
+
+    // get list of pages, input: ['title1', 'title2']
+    async getMultiple(data) {
+        return await Episodes.find({
+            title: {$in: data}
+        }, (err, ranks) => {
+            if (err) return new Error(err);
+            if (!ranks) return { message: 'no pages matched your criteria' };
+            return ranks;
+        })
+    }
+    
+    async getAll() {
+        return await PageRanks.find({}, (err, ranks) => {
+            if (err) return new Error(err);
+            if (!ranks) return { message: 'PageRank collection empty. Scraping should be started...' };
+            return ranks;
+        })
+    }
+
+    async getByTitle(title) {
+        return await PageRanks.findOne({title: title}, (err, rank) => {
+            if (err) return new Error(err);
+            if (!rank) return { message: 'no page matched your criteria' };
+            return rank;
+        })
+    }
+
+}
+module.exports = PageRankStore;
--- app/stores/fandom/religions.js
+++ app/stores/fandom/religions.js
@@ -1,1 +1,35 @@
-// TODO:
+const Religions = require('../../models/fandom/religions')
+
+class PageRankStore {
+    constructor() {}
+
+
+    // get list of pages, input: ['name1', 'name2']
+    async getMultiple(data) {
+        return await Religions.find({
+            name: {$in: data}
+        }, (err, rel) => {
+            if (err) return new Error(err);
+            if (!rel) return { message: 'No religions matched your criteria' };
+            return rel;
+        })
+    }
+    
+    async getAll() {
+        return await Religions.find({}, (err, rels) => {
+            if (err) return new Error(err);
+            if (!rels) return { message: 'Religion collection empty. Has scraping been started yet?' };
+            return rels;
+        })
+    }
+
+    async getByTitle(name) {
+        return await Religions.findOne({name: name}, (err, rel) => {
+            if (err) return new Error(err);
+            if (!rel) return { message: 'No religion matched your criteria' };
+            return rel;
+        })
+    }
+
+}
+module.exports = ReligionStore;
