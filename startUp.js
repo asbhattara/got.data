3ec0f388c8f1f9506351e1acc0143251b48741c0
Fabian Emilius <fabian.emilius@gmail.com>
2019-04-02 16:37:01 +0200

    - added post request authentication - removed grunt - renamed startup to app.js

--- startUp.js
+++ startUp.js
@@ -5,6 +5,8 @@ const config = require(__base + 'cfg/config');
 const UpdateFandom = require(__base + '/scripts/updateFandom');
 const UpdateWesteros = require(__base + '/scripts/updateWesteros');
 
+const uuidv4 = require('uuid/v4');
+
 function getDbString(config) { //Create the DB connection string
     let dbConnection = "mongodb://";
     if (config.username.length > 0 && config.password.length > 0) {
@@ -13,9 +15,44 @@ function getDbString(config) { //Create the DB connection string
     return dbConnection + config.uri + ":" + config.port + "/" + config.db;
 }
 
+function routerAuthentication(req, res, next) {
+    console.log('Request incoming: ' + req.url);
+
+    //Allow all GET requests as these do not modify data and we want users to be able to see that basic stuff
+    if (req.method === 'GET') {
+        return next();
+    }
+
+    //Otherwise check if we got a token
+    let sentToken = req.query.token;
+    if (!sentToken) {
+        console.log('401 - no token sent');
+        return res.status(401).send({ //Send a nice little message to remind the user that he needs to supply a token
+            message: 'Need to send a token',
+            code: 401
+        });
+    }
+
+    //Also check if the token is valid or not
+    if (sentToken == accessToken) {
+        return next();
+    } else {
+        console.log('401 - wrong token sent');
+        return res.sendStatus(401);
+    }
+}
+
+if (config.server.accessToken) {
+    global.accessToken = config.server.accessToken;
+} else {
+    global.accessToken = uuidv4(); //Generate a default token when none is set
+}
+
+console.log('Your requests must contain the following token: ' + accessToken);
+
 const express = require('express'),
     app = express(),
-    port = process.env.PORT || 3000,
+    port = config.server.port || 3000,
     mongoose = require('mongoose'),
     cors = require('cors'),
     bodyParser = require('body-parser');
@@ -54,6 +91,9 @@ app.use(cors({
 
 const showRouter = express.Router();
 const bookRouter = express.Router();
+
+showRouter.use(routerAuthentication);
+bookRouter.use(routerAuthentication);
 
 require('./app/routes/fandomRoutes')(app, showRouter);
 require('./app/routes/westerosRoutes')(app, bookRouter);
